9fa76d59b258c0dd87c8c5e945cc2a6ef6690d69
ASDerevyanko <ASDerevyanko@1cbit.ru>
2020-11-03 12:14:02 +0300

    test

---
+++ company/new1.php
@@ -0,0 +1,4 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("new1");
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Мебельная компания");
+?><?$APPLICATION->IncludeComponent("bitrix:breadcrumb", "universal", array(
+	"COMPONENT_TEMPLATE" => "universal",
+		"PATH" => "",
+		"SITE_ID" => "s1",
+		"START_FROM" => "0"
+	),
+	false,
+	array(
+	"ACTIVE_COMPONENT" => "N"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
